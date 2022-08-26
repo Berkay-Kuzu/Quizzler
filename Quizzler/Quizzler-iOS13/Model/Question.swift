5e49615daf277c4c6acd8a6d8108f7394932ffa2
Berkay Kuzu <92036779+Berkay-Kuzu@users.noreply.github.com>
2022-08-26 21:52:14 +0300

    Add files via upload

---
+++ Quizzler/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,15 @@
+
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
+    
